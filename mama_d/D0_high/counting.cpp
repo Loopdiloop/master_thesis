1451cf32c7fcb46634d5d46f3a65765894a3de0a
loopdiloop <mamoll@uio.no>
2021-04-20 23:27:15 +0200

    mama_d

---
+++ mama_d/D0_high/counting.cpp
@@ -0,0 +1,88 @@
+{
+   gROOT->Reset();
+   gROOT->SetStyle("Plain");
+   gStyle->SetOptTitle(0);
+   gStyle->SetOptStat(0);
+   gStyle->SetFillColor(0);
+   gStyle->SetPadBorderMode(0);
+   m = (TH1F*)gROOT->FindObject("h");
+   if (m) m->Delete();
+   TCanvas *c1 = new TCanvas("c1","Normalization of level density",600,600);
+   TH2F *h = new TH2F("h"," ",10,-0.887500,6.304500,50,4.587798,132554120.000000);
+   ifstream rholev("rholev.cnt"), rhopaw("rhopaw.cnt"), fermi("fermigas.cnt");
+   float levels[30],rho[30],rhoerr[30],energy[405],energyerr[405],fermigas[405];
+   float Bn[1]={5.871000};
+   float Bnerr[1]={0.001};
+   float rho_Bn[1]={4487000.000000};
+   float rho_Bnerr[1]={156000.000000};
+   int i = 0;
+   float a0 =  -0.8875;
+   float a1 =   0.2480;
+   float x,y,z;
+   while(fermi){
+   	fermi >> x;
+   	fermigas[i]=x;
+   	energy[i]=a0+(a1*i);
+   	energyerr[i]=0.0;
+      i++;
+   }
+   i=0;
+   while(rhopaw){
+   	rhopaw >> y;
+   	if(i<29){
+   		rho[i]=y;
+   	}
+   	else{rhoerr[i-29]=y;}
+   	i++;
+   }
+  	i=0;
+	while(rholev){
+		rholev >> z;
+		levels[i]=z;
+		i++;
+  }
+   TGraphErrors *rhoexp = new TGraphErrors(29,energy,rho,energyerr,rhoerr);
+   TGraphErrors *rhoBn = new TGraphErrors(1,Bn,rho_Bn,Bnerr,rho_Bnerr);
+   TGraph *fermicalc = new TGraph(404,energy,fermigas);
+   TGraph *level = new TGraph(29,energy,levels);
+   c1->SetLogy();
+   c1->SetLeftMargin(0.14);
+   h->GetXaxis()->CenterTitle();
+   h->GetXaxis()->SetTitle("Excitation energy E (MeV)");
+   h->GetYaxis()->CenterTitle();
+   h->GetYaxis()->SetTitleOffset(1.4);
+   h->GetYaxis()->SetTitle("Level density #rho (E) (MeV^{-1})");
+   h->Draw();
+   rhoexp->SetMarkerStyle(21);   rhoexp->SetMarkerSize(0.8);
+   rhoexp->Draw("P");
+   fermicalc->SetLineStyle(2);
+   fermicalc->DrawGraph(16,&fermicalc->GetX()[14],&fermicalc->GetY()[14],"L");
+   level->SetLineStyle(1);
+   level->Draw("L");
+   rhoBn->SetMarkerStyle(25);
+   rhoBn->SetMarkerSize(0.8);
+   rhoBn->Draw("P");
+   TLegend *leg = new TLegend(0.15,0.70,0.6,0.85);
+   leg->SetBorderSize(0);
+   leg->SetFillColor(0);
+   leg->AddEntry(rhoexp," Oslo data ","P");
+   leg->AddEntry(level," Known levels ","L");
+   leg->AddEntry(fermicalc," CT or FG model ","L");	
+   leg->AddEntry(rhoBn," #rho from neutron res. data ","P");
+   leg->Draw();
+   TLatex t;
+   t.SetTextSize(0.05);
+   t.DrawLatex(    5.044,6.628e+07,"^{xx}Yy");
+   TArrow *arrow1 = new TArrow(0.104500,678.573679,0.104500,116.520248,0.02,">");
+   arrow1->Draw();
+   TArrow *arrow2 = new TArrow(0.848500,9039.098169,0.848500,1552.135010,0.02,">");
+   arrow2->Draw();
+   TArrow *arrow3 = new TArrow(3.576500,2422063.220277,3.576500,415900.906250,0.02,">");
+   arrow3->Draw();
+   TArrow *arrow4 = new TArrow(5.064500,56911862.452340,5.064500,9772534.000000,0.02,">");
+   arrow4->Draw();
+   c1->Update();
+   c1->Print("counting.pdf");
+   c1->Print("counting.eps");
+   c1->Print("counting.ps");
+}
---
+++ mama_d/gamma_low/sigext.cpp
@@ -0,0 +1,73 @@
+{
+   gROOT->Reset();
+   gROOT->SetStyle("Plain");
+   gStyle->SetOptTitle(0);
+   gStyle->SetOptStat(0);
+   gStyle->SetFillColor(0);
+   gStyle->SetPadBorderMode(0);
+   m = (TH1F*)gROOT->FindObject("h");
+   if (m) m->Delete();
+   TCanvas *c1 = new TCanvas("c1","Normalization of gamma-transmission coefficient",600,600);
+   TH2F *h = new TH2F("h"," ",10,-0.887500,   7.056,50,2.302e+01,9.198e+07);
+   ifstream sigfile("sigpaw.cnt");
+   float sig[30],sigerr[30];
+   float energy[81],energyerr[81];
+   float extL[82],extH[82];
+   int i;
+   float a0 = -0.8875;
+   float a1 =  0.2480;
+   for(i = 0; i < 30; i++){
+   	energy[i] = a0 + (a1*i);
+   	energyerr[i] = 0.0;
+   	extL[i] = 0.0;
+   	extH[i] = 0.0;
+   }
+   float x, y;
+   i = 0;
+   while(sigfile){
+   	sigfile >> x;
+   	if(i<29){
+   		sig[i]=x;
+   	}
+   	else{sigerr[i-29]=x;}
+   	i++;
+   }
+   ifstream extendfile("extendLH.cnt");
+   i = 0;
+   while(extendfile){
+   	extendfile >> x >> y ;
+   	extL[i]=x;
+   	extH[i]=y;
+   	i++;
+   }
+   TGraph *extLgraph = new TGraph(30,energy,extL);
+   TGraph *extHgraph = new TGraph(30,energy,extH);
+   TGraphErrors *sigexp = new TGraphErrors(29,energy,sig,energyerr,sigerr);
+   c1->SetLogy();
+   c1->SetLeftMargin(0.14);
+   h->GetXaxis()->CenterTitle();
+   h->GetXaxis()->SetTitle("#gamma-ray energy E_{#gamma} (MeV)");
+   h->GetYaxis()->CenterTitle();
+   h->GetYaxis()->SetTitleOffset(1.4);
+   h->GetYaxis()->SetTitle("Transmission coeff. (arb. units)");
+   h->Draw();
+   sigexp->SetMarkerStyle(21);
+   sigexp->SetMarkerSize(0.8);
+   sigexp->Draw("P");
+   extLgraph->SetLineStyle(1);
+   extLgraph->DrawGraph(10,&extLgraph->GetX()[0],&extLgraph->GetY()[0],"L");
+   extHgraph->SetLineStyle(1);
+   extHgraph->DrawGraph(10,&extHgraph->GetX()[20],&extHgraph->GetY()[20],"L");
+   TArrow *arrow1 = new TArrow(8.485e-01,9.014e+03,8.485e-01,1.441e+03,0.02,">");
+   arrow1->Draw();
+   TArrow *arrow2 = new TArrow(1.344e+00,4.902e+04,1.344e+00,7.834e+03,0.02,">");
+   arrow2->Draw();
+   TArrow *arrow3 = new TArrow(4.073e+00,6.455e+06,4.073e+00,1.032e+06,0.02,">");
+   arrow3->Draw();
+   TArrow *arrow4 = new TArrow(4.817e+00,1.747e+07,4.817e+00,2.792e+06,0.02,">");
+   arrow4->Draw();
+   c1->Update();
+   c1->Print("sigext.pdf");
+   c1->Print("sigext.eps");
+   c1->Print("sigext.ps");
+}
